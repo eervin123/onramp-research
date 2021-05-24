ae0262c4029a7dd1bbb0964cbc7587b135342a98
Eric Ervin <eervin@realityshares.com>
2021-05-24 09:29:18 -0700

    changed app title for browser tabs

--- index.py
+++ index.py
@@ -3,31 +3,36 @@ import dash_html_components as html
 import dash
 
 from dash_app import dash_app
+
 # from dash_app import server #Eric modified to move in code for gunicorn
 from layouts import dashboard_page, vol_page, heatmap_page, heatmap_timeline_page
 import callbacks
 
-server = dash_app.server # Eric moved this here after struggling with gunicorn
+server = dash_app.server  # Eric moved this here after struggling with gunicorn
 
-dash_app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
+dash_app.layout = html.Div(
+    [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
+)
 
-dash_app.title = 'Onramp Research'
-@dash_app.callback(dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
+dash_app.title = "Onramp Academy Tools"
+
+
+@dash_app.callback(
+    dash.dependencies.Output("page-content", "children"),
+    [dash.dependencies.Input("url", "pathname")],
+)
 def display_page(pathname):
-    if pathname == '/apps/dashboard':
-         return dashboard_page
-    elif pathname == '/apps/volatility-chart':
-         return vol_page
-    elif pathname == '/apps/correlation-matrix':
-         return heatmap_page
-    elif pathname == '/apps/correlation-timeline':
-         return heatmap_timeline_page
+    if pathname == "/apps/dashboard":
+        return dashboard_page
+    elif pathname == "/apps/volatility-chart":
+        return vol_page
+    elif pathname == "/apps/correlation-matrix":
+        return heatmap_page
+    elif pathname == "/apps/correlation-timeline":
+        return heatmap_timeline_page
     else:
-        return dashboard_page # This is the "home page"
+        return dashboard_page  # This is the "home page"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     dash_app.run_server(debug=True)
